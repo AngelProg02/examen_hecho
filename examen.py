8de0808edc1e094f8dfa37b0e9351005793e15f3
AngelProg02 <angelluisvn@gmail.com>
2024-11-06 08:58:03 +0100

    Correción examen, utilización del width

--- examen.py
+++ examen.py
@@ -4,18 +4,20 @@ canciones = {}
 
 def cargar_lista(nombre_archivo):
    
+    diccionario = {}
     argumento = nombre_archivo + ".txt"
-    fichero = open(argumento,"r")
-   
-    for linea in fichero:
-        cancion, artista = linea.strip().split("-")
-        canciones[cancion] = artista.strip()
+    with open(argumento, "r") as fichero:
     
-    fichero.close()
+        for linea in fichero:
+            cancion, artista = linea.strip().split("-")
+            diccionario[cancion] = artista.strip()
+    
+    return diccionario
+  
 
 def agregar_cancion(diccionario_canciones,cancion,artista):
 
-    diccionario_canciones[artista] = cancion
+    diccionario_canciones[cancion] = artista
     
     print("Artista agregado: " , diccionario_canciones.items())
 
@@ -28,14 +30,14 @@ def eliminar_cancion(diccionario_canciones,titulo_canción):
         print("Canción eliminada")
         print(diccionario_canciones)
     else:
-        print("No existente en el diccionario")
+        print("No existe en el diccionario")
 
 def ordenar_alfabeticamente(diccionario_canciones):
 
     #devuelve clave-valor, cancion-artista
     list_diccionario = list(diccionario_canciones.items())
     
-    #ordena por el primer valor, en este caso, por la cancíon, sino sería otro índice[0]
+    #ordena por el primer valor, en este caso, por la cancíon
     list_diccionario.sort(key= lambda cancion_artista : cancion_artista[1])
 
     return list_diccionario
@@ -49,28 +51,11 @@ def contar_canciones(diccionario_canciones):
     return total_salida
 
 def crear_lista_aleatoria(diccionario_canciones,num_can_aleatorias):
-
-    max_diccionario = len(diccionario_canciones)
-    canciones_listadas = list(diccionario_canciones.keys())
-    lista_canciones = []
-    contador = 0
-
-    if num_can_aleatorias > max_diccionario:
-
-        print("No se encuentran tantas canciones dentro del diccionario de canciones")
-        num_can_aleatorias = max_diccionario
-
     
-    while contador < num_can_aleatorias:
-
-        numero_aleatorio = random.randint(0,max_diccionario-1)
-        cancion_aleatoria = canciones_listadas[numero_aleatorio]
-        lista_canciones.append(cancion_aleatoria)
-        contador += 1
+    n = min(num_can_aleatorias, len(diccionario_canciones))
+    lista = random.sample(diccionario_canciones.items(),n)
     
-    print(lista_canciones)
-    
-    return lista_canciones
+    return lista
 
 def guardar_lista(diccionario_canciones,nombre):
 
